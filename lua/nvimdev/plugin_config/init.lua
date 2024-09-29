0e33d256590610d9010700896ec1566ce9fc352a
prrockzed <priyanshuranjan2806@gmail.com>
2024-09-29 19:00:46 +0530

    Added lualine to manage statusline
    Added the configuration too

--- lua/nvimdev/plugin_config/init.lua
+++ lua/nvimdev/plugin_config/init.lua
@@ -7,3 +7,4 @@ require("nvimdev.plugin_config.color_config")
 -- FILES
 require("nvimdev.plugin_config.nvimtree")
 require("nvimdev.plugin_config.barbar")
+require("nvimdev.plugin_config.lualine")
--- lua/nvimdev/plugins/dump_plugins.lua
+++ lua/nvimdev/plugins/dump_plugins.lua
@@ -34,6 +34,15 @@ local plugins_list = {
 		},
 		version = "^1.0.0", -- optional: only update when a new 1.x version is released
 	},
+
+	-- status line at the bottom
+	{
+		"nvim-lualine/lualine.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
 
 return plugins_list
