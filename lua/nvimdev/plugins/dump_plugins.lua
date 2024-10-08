83a6cbe341b7cd50601f44b171369a729da55d85
Sayak Banerjee <69157316+gits-up@users.noreply.github.com>
2024-10-08 22:22:00 +0530

    Updated plugins and keymaps

--- lua/nvimdev/plugins/dump_plugins.lua
+++ lua/nvimdev/plugins/dump_plugins.lua
@@ -1,96 +1,154 @@
 -- dumping all the plugins here
 
 local plugins_list = {
-  -- which-key plugin
-  {
-    "folke/which-key.nvim",
-    version = "v1.6.0",
-  },
-  { 
-    "folke/neoconf.nvim",
-    cmd = "Neoconf"
-  },
-  {
-    "folke/lazydev.nvim",
-  },
 
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-live-grep-args.nvim" ,},
+	-- Plugin manager: Lazy.nvim
+	{ 
+	  "folke/lazy.nvim",
+	  version = "latest", -- Always keep it updated
 	},
-	    {
-		"nvim-telescope/telescope-ui-select.nvim", -- sets vim.ui.select to telescope
-		event = "VeryLazy",
-	},
-
-	-- find projects across the machine
+  
+	-- Which-key for keybinding suggestions
 	{
-		"ahmedkhalf/project.nvim",
-		lazy = false,
+	  "folke/which-key.nvim",
+	  version = "v1.6.0",
+	  config = function()
+		require("which-key").setup {}
+	  end,
 	},
-
-	-- tabline at the top
-	{
-		"romgrk/barbar.nvim",
-		lazy = false,
-		dependencies = {
-			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-		},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
-	},
-
-	-- status line at the bottom
-	{
-		"nvim-lualine/lualine.nvim",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
-
-  -- colorscheme plugins
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-  },
-	{
-		"EdenEast/nightfox.nvim",
-		event = "VeryLazy",
+  
+	-- Neovim configuration helper
+	{ 
+	  "folke/neoconf.nvim",
+	  cmd = "Neoconf",
 	},
 	{
-		"folke/tokyonight.nvim",
-		event = "VeryLazy",
+	  "folke/lazydev.nvim",
+	},
+  
+	-- Telescope and extensions for fuzzy searching
+	{
+	  "nvim-telescope/telescope.nvim",
+	  dependencies = { "nvim-lua/plenary.nvim" },
+	  lazy = false,
+	  config = function()
+		require("telescope").setup {}
+	  end,
 	},
 	{
-		"romgrk/doom-one.vim",
-		event = "VeryLazy",
+	  "nvim-telescope/telescope-live-grep-args.nvim",
+	  event = "VeryLazy", -- Load when required
+	  config = function()
+		require("telescope").load_extension("live_grep_args")
+	  end,
 	},
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		event = "VeryLazy",
+	  "nvim-telescope/telescope-ui-select.nvim", -- Overwrites vim.ui.select with telescope
+	  event = "VeryLazy",
+	  config = function()
+		require("telescope").load_extension("ui-select")
+	  end,
+	},
+  
+	-- Project management
+	{
+	  "ahmedkhalf/project.nvim",
+	  lazy = false,
+	  config = function()
+		require("project_nvim").setup {}
+	  end,
+	},
+  
+	-- Tabline plugin for better buffer/tab management
+	{
+	  "romgrk/barbar.nvim",
+	  lazy = false,
+	  dependencies = {
+		"lewis6991/gitsigns.nvim", -- Git status integration
+		"nvim-tree/nvim-web-devicons", -- File icons
+	  },
+	  version = "^1.0.0",
+	  config = function()
+		require("barbar").setup {}
+	  end,
+	},
+  
+	-- Status line at the bottom of the editor
+	{
+	  "nvim-lualine/lualine.nvim",
+	  lazy = false,
+	  dependencies = { "nvim-tree/nvim-web-devicons" }, -- For icons
+	  config = function()
+		require("lualine").setup {
+		  options = {
+			theme = "auto", -- Automatically adapts to your colorscheme
+		  },
+		}
+	  end,
+	},
+  
+	-- Colorschemes
+	{
+	  "olimorris/onedarkpro.nvim", -- Default colorscheme
+	  priority = 1000, -- Ensure it loads first
+	  config = function()
+		vim.cmd([[colorscheme onedarkpro]])
+	  end,
 	},
 	{
-		"rebelot/kanagawa.nvim",
-		event = "VeryLazy",
+	  "EdenEast/nightfox.nvim",
+	  event = "VeryLazy",
 	},
 	{
-		"ellisonleao/gruvbox.nvim",
-		event = "VeryLazy",
+	  "folke/tokyonight.nvim",
+	  event = "VeryLazy",
+	  config = function()
+		require("tokyonight").setup({
+		  style = "night", -- Choose between "storm", "night", "day"
+		  transparent = false, -- Disable transparency
+		})
+	  end,
 	},
-
-	-- nvim tree (a file explorer)
 	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
+	  "romgrk/doom-one.vim",
+	  event = "VeryLazy",
 	},
+	{
+	  "catppuccin/nvim",
+	  name = "catppuccin",
+	  event = "VeryLazy",
+	  config = function()
+		require("catppuccin").setup({
+		  flavour = "macchiato", -- Options: latte, frappe, macchiato, mocha
+		})
+		vim.cmd([[colorscheme catppuccin]])
+	  end,
+	},
+	{
+	  "rebelot/kanagawa.nvim",
+	  event = "VeryLazy",
+	},
+	{
+	  "ellisonleao/gruvbox.nvim",
+	  event = "VeryLazy",
+	  config = function()
+		require("gruvbox").setup {
+		  contrast = "hard",
+		}
+	  end,
+	},
+  
+	-- Nvim-tree for file exploration
+	{
+	  "nvim-tree/nvim-tree.lua",
+	  version = "*",
+	  lazy = false,
+	  dependencies = { "nvim-tree/nvim-web-devicons" },
+	  config = function()
+		require("nvim-tree").setup {}
+	  end,
+	},
+  
 }
 
 return plugins_list
